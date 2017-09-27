7d54edeac467ef45fa69f24b0e0268239eb43ad3
VENKATESH108 <32321067+VENKATESH108@users.noreply.github.com>
2017-09-27 11:29:48 +0530

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Category,CategoryAdmin,Quiz,QuizAdmin,Answer,Question,Score
+# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(CategoryAdmin)
+admin.site.register(Quiz)
+admin.site.register(QuizAdmin)
+admin.site.register(Answer)
+admin.site.register(Question)
+admin.site.register(Score)
+
+
+
+
+
+
+
+
